4a2447982624459f243d9e12956525acd8a76b76
inhui8 <yis027@ucsd.edu>
2021-11-14 20:29:26 -0800

    done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Lab 8 - Starter
+Yinghui Sun
+1) I choose number 1, Within a Github action that runs whenever code is pushed . 
+   I think it will be the best that runs in github since all the people have the same test
+   and since the test we can know what is the actully prlblems, it we set test in our computer
+   but sometimes it will delete something after test finished that we do not know, it cause problem, 
+   it can also help us better to manage the pull request.
+
+2) No
+3) No, actully I think the message they want to send user to user, We can not test what actually message  format they want to sent
+   also it should test did user has the event of send message when they click send
+4) Yes, we can test the max length of message by unit test, because we can easily to write a test for that, and unit test can 
+   catch the problems if user achives the max message length
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,33 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('phone',() => {
+    expect(functions.isPhoneNumber('500-500-5500')).toBe(true);
+    expect(functions.isPhoneNumber('400-400-4400')).toBe(true);
+    expect(functions.isPhoneNumber('sdfas32234')).toBe(false);
+    expect(functions.isPhoneNumber('2345234534')).toBe(false);
+});
+test('email',() => {
+    expect(functions.isEmail('google@gmail.com')).toBe(true);
+    expect(functions.isEmail('google@ucsd.edu')).toBe(true);
+    expect(functions.isEmail('google')).toBe(false);
+    expect(functions.isEmail('nameiii1231@@gmail')).toBe(false);
+});
+test('password',() => {
+    expect(functions.isStrongPassword('Ucsd9999')).toBe(true);
+    expect(functions.isStrongPassword('SSSsss1122')).toBe(true);
+    expect(functions.isStrongPassword('__1234')).toBe(false);
+    expect(functions.isStrongPassword('asdfasdfasdfasdfadf')).toBe(false);
+});
+test('date',() => {
+    expect(functions.isDate('11/14/2021')).toBe(true);
+    expect(functions.isDate('01/01/2012')).toBe(true);
+    expect(functions.isDate('Oct/twentyth/2012')).toBe(false);
+    expect(functions.isDate('02/02/20')).toBe(false);
+});
+test('hex',() => {
+    expect(functions.isHexColor('000080')).toBe(true);
+    expect(functions.isHexColor('FF00FF')).toBe(true);
+    expect(functions.isHexColor('adsfasasd')).toBe(false);
+    expect(functions.isHexColor('s')).toBe(false);
+});
